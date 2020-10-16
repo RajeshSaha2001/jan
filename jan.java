5979cab49e0f10bbe5588b2eb69edf4055eb479a
Rajesh Chandra Saha <55900566+RajeshSaha2001@users.noreply.github.com>
2020-10-16 18:32:58 +0530

    Add files via upload

---
+++ jan.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class c_compareTo
+{
+
+    /* int compareTo(String anotherString)
+    Compares two st
+        
+        if(s1.compareTo(s2)==0)
+        {
+        else
+        {
+            System.out.println("Not Same");
+        }
+    }
+}
